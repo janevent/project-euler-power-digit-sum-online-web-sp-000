4d1c0bb37a7c11442eb5b19015caa8e8c1c4d516
Jane Venter <janeanneventer@gmail.com>
2020-09-25 21:35:58 +0000

    Automatically backed up by Learn

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,4 +1,7 @@
 def power_digit_sum(x, n)
   answer = x^n 
-  answer.to_s.split('').reduce(0){|sum, num| sum + num.to_i }
+  puts answer
+  reduced_answer = answer.to_s.split('').reduce(0){|sum, num| sum + num.to_i }
+  puts reduced_answer 
+  reduced_answer
 end
